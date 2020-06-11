c5c6907de1cfbc57d785224ad9bff3ab3b625582
c0rekt <53911204+c0rekt@users.noreply.github.com>
2020-06-12 04:05:49 +0800

    Add files via upload

---
+++ ExcelToCsv.ps1
@@ -0,0 +1,40 @@
+﻿####################################
+#         Author:c0rekt            #
+#          version 1.0             #
+#    Excel to csv converter        #
+####################################
+
+function ExcelToCsv {
+    [cmdletbinding()]
+    Param (
+        [string]$ExcelFileName,
+        [string]$ExcelFilePath,
+        [string]$SheetName,
+        [string]$CsvLocation,
+        [string]$Delimiter,
+        [int]$RowStart
+    )
+    $excelfile = "$($ExcelFilePath)$($ExcelFileName)"
+    $excel = New-Object -ComObject Excel.Application
+    $excelWorkbook = $excel.Workbooks.Open("$excelfile")
+    $excelWorksheet = $excelWorkbook.Sheets.Item("$SheetName")
+    $WorksheetRange = $excelWorksheet.UsedRange
+    $RowCount = $WorksheetRange.Rows.Count
+    $ColumnCount = $WorksheetRange.Columns.Count
+
+    for($i = $RowStart; $i -le $RowCount; $i++){
+        for($j = 1; $j -le $ColumnCount; $j++){
+            $items =  $excelWorksheet.Columns.Item($j).Rows.Item($i).Text
+                if(!$items){
+                    $items = "NULL"
+                }
+                $output += "$($items)$($Delimiter)"
+            }
+            $StringLength = $output.Length
+            $output = $output.Substring(0,$StringLength-1) + "`n"
+    }
+    $OutputFileName = [io.path]::GetFileNameWithoutExtension($ExcelFileName)
+    Write-output "$($output)" >> "$($CsvLocation)$($OutputFileName).csv"
+}
+$excel.Workbooks.Close()
+Export-ModuleMember -Function * -Alias *
